86d1d9a190f0c7c1012b2f63e41f1120a4fae7c2
Nikita <ployka@yandex.ru>
2023-07-16 16:08:05 +0700

    start

---
+++ wfile/src/main/java/ru/skypro/lessons/wfile/wfile/WfileApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.wfile.wfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WfileApplication.class, args);
+    }
+
+}
